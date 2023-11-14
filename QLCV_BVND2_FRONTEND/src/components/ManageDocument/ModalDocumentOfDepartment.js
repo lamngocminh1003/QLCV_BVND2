d5b035dc9b51181a5004938148502ee5b825ba8a
unknown <phamtran8983@gmail.com>
2023-11-14 16:29:53 +0700

    14/11/2023

--- QLCV_BVND2_FRONTEND/src/components/ManageDocument/ModalDocumentOfDepartment.js
+++ QLCV_BVND2_FRONTEND/src/components/ManageDocument/ModalDocumentOfDepartment.js
@@ -4,18 +4,14 @@ import "./Document.scss";
 import _ from 'lodash';
 import { v4 as uuidv4 } from 'uuid';
 import "../Task/Task.scss";
-// import AccordionSummary from "@mui/material/AccordionSummary";
-// import Accordion from "@mui/material/Accordion";
-// import AccordionDetails from "@mui/material/AccordionDetails";
-// import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-// import {
-//     Box,
-//     List,
-//     ListItem,
-//     ListItemText,
-//     Typography,
-//     useTheme,
-//   } from "@mui/material";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import {Box, List, ListItem, ListItemText, Typography, useTheme} from "@mui/material";
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import { toast } from 'react-toastify';
 
 
 const ModalDocumentOfDepartment = (props) => {
@@ -56,11 +52,16 @@ const ModalDocumentOfDepartment = (props) => {
         setListTask(_listTask);
     }
 
-    const editTask = (name, value, itemKey) => {
-        
+    const editTask = (name, value, itemKey, event) => {
+        event.stopPropagation();
+        // let _listTask = _.cloneDeep(listTask);
+        // _listTask[itemKey][name] = value;
+        // setListTask(_listTask);
+        // event.stopPropagation();
     }
 
-    const delTask = (itemKey) => {
+    const delTask = (itemKey, event) => {
+        event.stopPropagation();
         let _listTask = _.cloneDeep(listTask); 
         delete _listTask[itemKey];
         setListTask(_listTask);
@@ -68,6 +69,7 @@ const ModalDocumentOfDepartment = (props) => {
 
     const saveTask = () => {
         //console.log(listTask);
+        toast.success("Đã lưu công việc!");
     }
 
     useEffect(() => {
@@ -106,25 +108,46 @@ const ModalDocumentOfDepartment = (props) => {
                             
                             <div className='list-task'>
                                 <h4 className="row text-primary text-uppercase mt-3">Danh sách công việc</h4>
-                                <div className='list-task-div'>
+                                <div className='list-task-div'>   
                                     {
                                         Object.entries(listTask).map(([itemKey, itemValue]) => {
                                             return(
                                                 <>
-                                                    <div className={`list-title form-control col-12 mt-3 d-flex justify-content-between py-2`} key={`task-${itemKey}`}> 
-                                                        <div className={`item child ${itemKey} text-uppercase text-white fw-bolder py-1 col-10`} 
-                                                            contentEditable={true} 
-                                                            style={{fontSize: '17px'}}
-                                                            onChange={(event) => editTask('taskName', event.target.value, itemKey)}
-                                                        >
-                                                            {itemValue.taskName}
+                                                    <Accordion className='list-title mt-3 rounded-3' key={`task-${itemKey}`} style={{boxShadow: 'none'}}>
+                                                        <div className='p-1'>
+                                                            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                                                                <Typography className='text-uppercase text-white fw-bolder' style={{fontSize: '17px'}} >
+                                                                    {itemValue.taskName}
+                                                                </Typography>
+                                                                <Box className='text-white d-flex ms-auto mr-1'>
+                                                                    <EditIcon className='mr-2' fontSize='medium' onClick={(event) => editTask('taskName', event.currentTarget.textContent, itemKey, event)} />
+                                                                    <DeleteIcon fontSize='medium' color="inherit" onClick={(event) => delTask(itemKey, event)}/>
+                                                                </Box>
+                                                            </AccordionSummary>
                                                         </div>
-                                                        <div className='icons d-flex' style={{fontSize: '20px'}}>
-                                                            <button className='mr-2'><i className="fa fa-pencil text-white" onClick={() => editTask()}></i></button>
-                                                            <button><i className="fa fa-times text-white" onClick={() => delTask(itemKey)}></i></button>
+                                                        <div className='list-child-task-of-title border border-top-0 rounded-bottom' style={{backgroundColor: "#fff"}}>
+                                                            <AccordionDetails>
+                                                                <Typography>
+                                                                    đây là phần tag người dùng và thảo luận (comment) về task đó cho người dùng
+                                                                </Typography>
+                                                            </AccordionDetails>
                                                         </div>
-                                                    </div>   
+                                                    </Accordion>
                                                 </>
+
+                                                // <div className={`list-title form-control col-12 mt-3 d-flex justify-content-between py-2`} key={`task-${itemKey}`}> 
+                                                //         <div className={`item child ${itemKey} text-uppercase text-white fw-bolder py-1 col-11`} 
+                                                //             contentEditable={true} 
+                                                //             style={{fontSize: '17px'}}
+                                                //             onBlur={(event) => editTask('taskName', event.currentTarget.textContent, itemKey)}
+                                                //         >
+                                                //             {itemValue.taskName}
+                                                //         </div>
+                                                //         <div className='icons d-flex' style={{fontSize: '20px'}}>
+                                                //             <button className='mr-2'><i className="fa fa-pencil text-white" onClick={() => editTask()}></i></button>
+                                                //             <button><i className="fa fa-times text-white" onClick={() => delTask(itemKey)}></i></button>
+                                                //         </div>
+                                                //     </div>   
                                             )
                                         })
                                     }
